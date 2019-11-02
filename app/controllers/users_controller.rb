c504672a01c175e6e3fa378c955e720aede18c30
Mrob513 <tatatiki@mail.ru>
2019-11-02 18:55:52 +0300

    заблокирована возможность редактирования пользователя и поста всем пользователям кроме, самого пользователья и админа

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :check_edit, only: [:edit, :update, :destroy]
-  
+
 
   # GET /users
   # GET /users.json
